a479c7d509362f5de47c712bce36e8671897fe7d
510208 <xux510208@gmail.com>
2025-11-02 20:49:47 +0800

    ✨ feat: 增加個人資訊回應，擴充預設路由處理

--- src/worker.js
+++ src/worker.js
@@ -7,6 +7,7 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+const CACHE_TTL = 3600; // seconds
 
 // worker.js
 export default {
@@ -18,7 +19,31 @@ export default {
 			const module = await import('./routes/wakatime.js');
 			return module.default.fetch(request, env, ctx);
 		}
-		// 其他路徑可擴充
-		return new Response('Not Found', { status: 404 });
+
+		// 預設回傳個人資訊
+		const taipeiYear = Number(new Intl.DateTimeFormat('en-US', { timeZone: 'Asia/Taipei', year: 'numeric' }).format(new Date()));
+
+		const personalInfo = {
+			name: 'SamHacker',
+			aka: ['510208', 'Misaka Itosana'],
+			age: taipeiYear - 2010,
+			location: 'Taichung, Taiwan',
+			links: [
+				{ github: 'https://github.com/510208' },
+				{ wakatime: 'https://wakatime.com/@SamHacker' },
+				{ blog: 'https://samhacker.xyz' },
+				{ personal_website: 'https://510208.github.io' },
+				{ bento: 'https://bento.me/510208' },
+				{ threads: 'https://www.threads.com/@samhacker.xyz' },
+				{ youtube: 'https://www.youtube.com/channel/UC6orwHdQNVzwHsA6M7HYD9g' },
+			],
+			school: 'Taichung Municipal Taichung First Senior High School',
+			hobbies: ['programming', 'reading', 'gaming', 'watching anime'],
+		};
+
+		return new Response(JSON.stringify(personalInfo), {
+			status: 200,
+			headers: { 'Content-Type': 'application/json' },
+		});
 	},
 };
